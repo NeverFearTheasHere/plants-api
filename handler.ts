ecd5b2bb13c3c580800c7eb5c6f9dfa0ac45cd67
Thea Lanherne <thea.lanherne@ghyston.com>
2020-03-25 09:45:13 +0000

    Add push notification API endpoints

--- handler.ts
+++ handler.ts
@@ -1,12 +1,91 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import { Expo } from 'expo-server-sdk';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+const expo = new Expo();
+const allPushTokens = ['ExponentPushToken[XVbCnmL44cuKw9CUG-_5La]'];
+
+export const registerPushToken: APIGatewayProxyHandler = async (event, _context) => {
+  const { body } = event
+  let pushToken;
+
+  const bodyJson = JSON.parse(body);
+  pushToken = bodyJson.pushToken;
+
+  allPushTokens.push(pushToken);
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+      message: `Registered push token: ${pushToken}`,
+    }),
   };
 }
+
+export const sendNotification: APIGatewayProxyHandler = async () => {
+  let messages = [];
+  for (let pushToken of allPushTokens) {
+    if (!Expo.isExpoPushToken(pushToken)) {
+      console.error(`Push token ${pushToken} is not a valid Expo push token`);
+      continue;
+    }
+
+    const plant = GetRandomPlant();
+
+    messages.push({
+      to: pushToken,
+      sound: 'default',
+      body: `Don't forget to water your ${plant.displayName.toLowerCase()}`,
+      data: { plantId: plant.id },
+    })
+  }
+
+  let chunks = expo.chunkPushNotifications(messages);
+
+  for (let chunk of chunks) {
+    try {
+      await expo.sendPushNotificationsAsync(chunk);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Sent ${messages.length} notifications`,
+      messages: messages,
+    }),
+  };
+}
+
+type Plant = {
+  id: number,
+  displayName: string,
+};
+
+const plants: Array<Plant> = [
+  {
+    id: 1,
+    displayName: 'Redcurrant',
+  },
+  {
+    id: 2,
+    displayName: 'Clematis',
+  },
+  {
+    id: 3,
+    displayName: 'New Zealand Myrtle',
+  },
+  {
+    id: 4,
+    displayName: 'Three-coloured Nasturtium',
+  },
+  {
+    id: 5,
+    displayName: `Kale 'Redbor'`,
+  },
+];
+
+const GetRandomPlant = () => plants[Math.floor(Math.random() * plants.length)];
+
